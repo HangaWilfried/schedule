1cd4d4fa243292b2f9d23e9a308ea1abcd3e7990
hanga.lagoue <hanga.lagoue@lao-sarl.cm>
2022-05-10 22:05:44 +0100

    implement teacher page

--- src/views/TeacherPage.vue
+++ src/views/TeacherPage.vue
@@ -1,27 +1,32 @@
 <template>
-  welcome to teacher page
-
-  <div class="w-full h-full inset-0 absolute bg-gray-300 divide-y-2">
-    <div>
-      <div>
-        <span>schedule</span>
-        <span>available from 09 may to 15 may 2022</span>
-      </div>
-      <div>
-        <div>
-          <button>licence 1</button>
-          <button>licence 2</button>
-          <button>licence 3</button>
-        </div>
-        <div>
-          <router-view></router-view>
-        </div>
+<div class="flex flex-col items-start w-full">
+    <div class="flex justify-between w-full shadow-md p-4">
+      <span class="text-xl text-gray-900 font-extrabold font-sans">schedule</span>
+      <div class="flex space-x-4 items-center text-xs">
+        <span class="bg-blue-100 rounded-md px-4 py-1">licence 3</span>
+        <span class="font-medium">available from 09 may to 15 may 2022</span>
       </div>
     </div>
-    <div class="absolute bottom-0 w-full">
-      teacher: DR NDJE.
+    <div class="w-full flex border">
+      <div class="flex flex-col py-2 px-4 space-y-2">
+        <button class="bg-blue-100 rounded-md px-4 py-1">licence 1</button>
+        <button class="bg-pink-100 rounded-md px-4 py-1">licence 2</button>
+        <button class="bg-orange-100 rounded-md px-4 py-1">licence 3</button>
+      </div>
+      <div class="border-l pl-10">
+        <router-view></router-view>
+      </div>
+    </div>
+    <div class="w-full pb-20 px-4 pt-4">
+      teacher : DR NDJE.
     </div>
   </div>
+
+
+
+
+
+
 </template>
 
 <script setup>
